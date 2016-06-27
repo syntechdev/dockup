9721b4bc3e71d01898073051927c99e0a774fc3c
Mohan Kartha <mckartha@gmail.com>
2016-06-27 01:15:22 -0400

    minor correction

--- s3dockup.sh
+++ s3dockup.sh
@@ -7,7 +7,7 @@ AWS_ACC_ID="`grep AWS_ACC s3dockup.env`" # ; echo AWS_ACC_ID: $AWS_ACC_ID
 AWS_SEC_KEY="`grep AWS_SEC s3dockup.env`" # ; echo AWS_SEC_KEY: $AWS_SEC_KEY
 S3_BUCKET=`grep S3_BUC s3dockup.env | sed "s/^S.*=//"` # ; echo S3_BUCKET: $S3_BUCKET
 
-docker run --rm --env-file s3dockup.env --volumes-from wink-ovpn -v /home/core:/home/core --name dockup tutum/dockup:latest
+docker run --rm --env-file s3dockup.env --volumes-from data-volume -v /home/core:/home/core --name dockup tutum/dockup:latest
 
 echo "*** Show contents of S3 Backup Bucket ***"
 echo
